63f202b948e2a50e253f3bea5cea766253862891
Gökhan Alıcı <gokhanaliccii@gmail.com>
2018-06-10 18:04:20 +0300

    Defined rent price table

--- src/main/java/com/gokhanaliccii/kata/RentalShop.java
+++ src/main/java/com/gokhanaliccii/kata/RentalShop.java
@@ -1,6 +1,15 @@
 package com.gokhanaliccii.kata;
 
+/**
+ * Movie Genres   New, Horror, Fantastic
+ * Price table
+ * New ----------> 4 lira for each day
+ * Horror -------> 2 lira for each day
+ * Fantastic ----> 6 lira for each day
+ */
 public class RentalShop {
+
+
     public double calculatePrice(int rentedDay) {
         return 0;
     }
