d48424c1262fa5ecc8fba0d4064fdca678945350
Gökhan Alıcı <gokhanaliccii@gmail.com>
2018-06-10 18:40:24 +0300

    Customer come with new idea. They want discount on rental price if user after rent more than defined day

--- src/test/java/com/gokhanaliccii/kata/RentalShopTest.java
+++ src/test/java/com/gokhanaliccii/kata/RentalShopTest.java
@@ -10,11 +10,25 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 /**
+ * [Step1]
  * Price table
  * New ----------> 4 lira for each day
  * Horror -------> 2 lira for each day
  * Comedy ----> 6 lira for each day
+ *
+ * [Step2]
+ * They came with new request. They decided
+ * discount if user's will rent movie more than specified
+ *
+ * Updated Price Table
+ * New------ More Than 3 day Apply %10 discount
+ * Horror--- More Than 5 day Apply %15 discount
+ * Comedy--- More Than 2 day Apply %3 discount
+ *
  */
+
+
+
 public class RentalShopTest {
 
     @Test
