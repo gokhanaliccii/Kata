bdc48af56167f3d0cc57c19ba032f66ef9fdd3cc
Gökhan Alıcı <gokhanaliccii@gmail.com>
2018-06-12 23:39:35 +0300

    Movie genres enum updated by new requirement

--- src/main/java/com/gokhanaliccii/kata/MovieGenres.java
+++ src/main/java/com/gokhanaliccii/kata/MovieGenres.java
@@ -1,54 +1,19 @@
 package com.gokhanaliccii.kata;
 
-public enum  MovieGenres {
-    NEW(4){
-        @Override
-        double calculatePrice(int rentedDay) {
-            double price = rentedDay * getRentalPrice();
+import java.util.function.BiFunction;
 
-            if (rentedDay > 3) {
-                price = price * 0.9;
-            }
+public enum MovieGenres {
+    NEW(MovieService::calculateComedyPrice),
+    HORROR(MovieService::calculateHorrorPrice),
+    COMEDY(MovieService::calculateComedyPrice);
 
-            return price;
-        }
-    }
-    , HORROR(2){
-        @Override
-        double calculatePrice(int rentedDay) {
-            double price = rentedDay * getRentalPrice();
-
-            if (rentedDay > 5) {
-                price = price * 0.85;
-            }
-
-            return price;
-        }
-    }
-    , COMEDY(6){
-        @Override
-        double calculatePrice(int rentedDay) {
-            double price = rentedDay * getRentalPrice();
-
-            if (rentedDay > 2) {
-                price = price * 0.7;
-            }
-
-            return price;
-        }
-    };
-
-    private final int rentPrice;
-
-    MovieGenres(int price){
-        this.rentPrice = price;
+    MovieGenres(BiFunction<MovieService, Integer, Double> algorithm) {
+        this.algorithm = algorithm;
     }
 
-    public int getRentalPrice() {
-        return rentPrice;
+    private final BiFunction<MovieService, Integer, Double> algorithm;
+
+    public BiFunction<MovieService, Integer, Double> getAlgorithm() {
+        return algorithm;
     }
-
-
-    abstract double calculatePrice(int rentedDay);
-
 }
