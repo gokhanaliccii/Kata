d09a0e9439720b9f940bf21ba4a061366228a8b9
alici,gokhan <gokhana.alici@accenture.com>
2018-07-12 17:20:41 +0300

    SequentialNumberFinder function variables renamed, indentations adjusted

--- src/main/java/com/gokhanaliccii/kata/algorithm/SequentialNumberFinder.java
+++ src/main/java/com/gokhanaliccii/kata/algorithm/SequentialNumberFinder.java
@@ -3,35 +3,31 @@ package com.gokhanaliccii.kata.algorithm;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class SequentialNumberFinder {
 
-
     public List<List<Integer>> findSequentialNumbers(int[] items, int limit) {
-        if (items.length == 0)
+        if (items == null || items.length == 0 || limit < 0) {
             return new ArrayList<>();
+        }
 
-        List<List<Integer>> consecutive = new LinkedList<>();
+        List<List<Integer>> sequentialNumbers = new LinkedList<>();
         List<Integer> tempList = new LinkedList<>();
 
-        for (int currentIndex = 0, nextIndex = currentIndex + 1;
-             nextIndex < items.length;
-             currentIndex++, nextIndex++) {
-
-            if (isConsecutive(items[currentIndex], items[nextIndex])) {
+        for (int currentIndex = 0, nextIndex = currentIndex + 1; nextIndex < items.length; currentIndex++, nextIndex++) {
+            if (isSequential(items[currentIndex], items[nextIndex])) {
                 tempList.add(items[currentIndex]);
 
                 if (isLastIndex(nextIndex, items.length)) {
                     tempList.add(items[nextIndex]);
                 }
             } else {
-
-                if (tempList.size() > 0)
+                if (tempList.size() > 0) {
                     tempList.add(items[currentIndex]);
+                }
 
                 if (tempList.size() >= limit) {
-                    consecutive.add(tempList);
+                    sequentialNumbers.add(tempList);
                 }
 
                 tempList = new LinkedList<>();
@@ -39,21 +35,17 @@ public class SequentialNumberFinder {
         }
 
         if (tempList.size() >= limit) {
-            consecutive.add(tempList);
+            sequentialNumbers.add(tempList);
         }
 
-        return consecutive;
+        return sequentialNumbers;
     }
 
-    private boolean isConsecutive(int num1, int num2) {
+    private boolean isSequential(int num1, int num2) {
         return num1 - (num2 - 1) == 0;
     }
 
     private boolean isLastIndex(int index, int size) {
         return index + 1 == size;
-    }
-
-    private int[][] emptyArray() {
-        return new int[0][0];
     }
 }
--- src/test/java/com/gokhanaliccii/kata/algorithm/SequentialNumberFinderTest.java
+++ src/test/java/com/gokhanaliccii/kata/algorithm/SequentialNumberFinderTest.java
@@ -2,22 +2,27 @@ package com.gokhanaliccii.kata.algorithm;
 
 import com.gokhanaliccii.kata.util.IsNestedListSize;
 import org.hamcrest.core.Is;
-import org.hamcrest.core.IsCollectionContaining;
+import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class SequentialNumberFinderTest {
+
+    private SequentialNumberFinder finder;
+
+    @Before
+    public void setUp() {
+        finder = new SequentialNumberFinder();
+    }
 
     @Test
     public void should_NotFindSequentialNumbersWhenInputEmpty() {
         int[] items = new int[0];
 
-        SequentialNumberFinder finder = new SequentialNumberFinder();
         assertThat(finder.findSequentialNumbers(items, 3), Is.is(new IsNestedListSize(0)));
     }
 
@@ -25,7 +30,6 @@ public class SequentialNumberFinderTest {
     public void should_SequentialNumbersReturnEmptyListWhenNotFoundEnoughSequential() {
         int[] items = new int[]{1, 2, 6, 7, 9};
 
-        SequentialNumberFinder finder = new SequentialNumberFinder();
         assertThat(finder.findSequentialNumbers(items, 3), Is.is(new IsNestedListSize(0)));
     }
 
@@ -33,40 +37,34 @@ public class SequentialNumberFinderTest {
     public void should_FindSequentialNumbersListItemCountCorrectly() {
         int[] items = new int[]{1, 2, 6, 7, 9};
 
-        SequentialNumberFinder finder = new SequentialNumberFinder();
         assertThat(finder.findSequentialNumbers(items, 2), Is.is(new IsNestedListSize(2)));
     }
 
     @Test
     public void should_FindSequentialPartsFromInputCorrectly() {
-        //int[] items = new int[]{ 17, 19, 20, 21};
         int[] items = new int[]{1, 2, 6, 7, 8, 9, 11, 14, 17, 19, 20, 21};
         List<List<Integer>> expectedItem = Arrays.asList(
                 Arrays.asList(6, 7, 8, 9),
                 Arrays.asList(19, 20, 21));
 
-        SequentialNumberFinder finder = new SequentialNumberFinder();
         assertEquals(expectedItem, finder.findSequentialNumbers(items, 3));
     }
 
     @Test
     public void should_FindSequentialPartsFromInputCorrectly2() {
-        //int[] items = new int[]{ 17, 19, 20, 21};
         int[] items = new int[]{1, 2, 6, 7, 8, 9, 11, 14, 17, 19, 20};
         List<List<Integer>> expectedItem = Arrays.asList(
                 Arrays.asList(6, 7, 8, 9));
 
-        SequentialNumberFinder finder = new SequentialNumberFinder();
         assertEquals(expectedItem, finder.findSequentialNumbers(items, 3));
     }
 
     @Test
-    public void should_FindSequentialPartsFromInputCorrectly3() {
+    public void should_FindSequentialPartsWhenInputSequentialCorrectly3() {
         int[] items = new int[]{1, 2, 3};
         List<List<Integer>> expectedItem = Arrays.asList(
                 Arrays.asList(1, 2, 3));
 
-        SequentialNumberFinder finder = new SequentialNumberFinder();
         assertEquals(expectedItem, finder.findSequentialNumbers(items, 1));
     }
 
